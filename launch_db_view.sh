5e9c9147db544e7277c05666f572bb6c08ca7696
Fabrizio Damicelli <fabridamicelli@gmail.com>
2026-05-01 18:35:18 +0200

    fix launch view

--- launch_db_view.sh
+++ launch_db_view.sh
@@ -2,19 +2,19 @@
 set -euo pipefail
 
 uv run sqlite-utils insert log.db logs log.csv --csv
-# Launch Datasette locally
-lsof -ti :8001 
 
 PORT=8001
-PID=$(lsof -ti :$PORT)
+PID=$(lsof -ti :$PORT || true)
 if [ -n "$PID" ]; then
+  echo "in loop"
   kill "$PID"
   echo "Killed process $PID running on port $PORT."
+else
+  echo "No process running on port $PORT, continue."
 fi
 
 while ss -ltn | grep -q ":$PORT "; do
   echo "Port $PORT is still in use. Waiting..."
   sleep 0.1
 done
-
 uv run datasette log.db
